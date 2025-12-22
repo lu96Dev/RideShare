219b043bbaf3c6357d3251fbe5bad3060596da02
AlvaroPG3 <perezgongoraalvaro@gmail.com>
2025-12-22 18:09:55 +0100

    Add files via upload

---
+++ backend/src/main/java/com/rideshare/backend/RideshareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rideshare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideshareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideshareBackendApplication.class, args);
+	}
+
+}
