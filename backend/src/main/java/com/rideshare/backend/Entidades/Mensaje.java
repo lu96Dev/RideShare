fd69c673836562373b21b5098da0146480aa58b4
6LUV6 <lmr0058@alu.medac.es>
2025-12-24 14:07:41 +0100

    Creando entidades

--- backend/src/main/java/com/rideshare/backend/Entidades/Mensaje.java
+++ backend/src/main/java/com/rideshare/backend/Entidades/Mensaje.java
@@ -1,4 +1,56 @@
 package com.rideshare.backend.Entidades;
 
 public class Mensaje {
+    String id;
+    String texto;
+    String fechaHora;
+
+    public Mensaje(){
+
+    }
+    public Mensaje(String id, String texto, String fechaHora) {
+        this.id = id;
+        this.texto = texto;
+        this.fechaHora = fechaHora;
+    }
+
+    public void enviar(){
+
+    }
+    public void eliminar(){
+
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(String fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Mensaje{" +
+                "id='" + id + '\'' +
+                ", texto='" + texto + '\'' +
+                ", fechaHora='" + fechaHora + '\'' +
+                '}';
+    }
 }
