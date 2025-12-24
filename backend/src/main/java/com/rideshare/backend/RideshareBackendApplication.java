96ee441473fb5b829110eefba9d26b5b1ce112a1
AlvaroPG3 <perezgongoraalvaro@gmail.com>
2025-12-24 12:24:37 +0100

    Testeo Endpoint

--- backend/src/main/java/com/rideshare/backend/RideshareBackendApplication.java
+++ backend/src/main/java/com/rideshare/backend/RideshareBackendApplication.java
@@ -1,4 +1,4 @@
-package com.rideshare.backend.Aplicacion;
+package com.rideshare.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
