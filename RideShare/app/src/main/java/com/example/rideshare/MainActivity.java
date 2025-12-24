8d87a4e7bca5499e81128e2e49f094a85d51bea0
AlvaroPG3 <perezgongoraalvaro@gmail.com>
2025-12-24 18:29:23 +0100

    Pantalla Principal y de Login creadas en Android Studio

--- RideShare/app/src/main/java/com/example/rideshare/MainActivity.java
+++ RideShare/app/src/main/java/com/example/rideshare/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.rideshare;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button botonLoginMain = findViewById(R.id.botonLoginMain);
+        TextView textoRegister = findViewById(R.id.textoMain2);
+
+        botonLoginMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(login);
+            }
+        });
+
+        textoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent register = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(register);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
