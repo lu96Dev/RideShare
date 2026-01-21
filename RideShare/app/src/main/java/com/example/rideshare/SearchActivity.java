62554a5ee6b3340957f09287c7416e2bfa9cf2d3
Kevin_Rubio22 <kevinjoserg4c@gmail.com>
2026-01-21 14:41:18 +0100

    Correción de errores y cambios entre Clase Fragments y Activitys

--- RideShare/app/src/main/java/com/example/rideshare/SearchActivity.java
+++ RideShare/app/src/main/java/com/example/rideshare/SearchActivity.java
@@ -1,37 +1,19 @@
 package com.example.rideshare;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class LoginActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        Button botonLogin = findViewById(R.id.botonLoginMain);
-
-        botonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SearchActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        setContentView(R.layout.activity_search);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
